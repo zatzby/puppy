ccde1253a683a76d2e9f1cd3cc85eb5b730d5b9c
zatzby <128881293+zatzby@users.noreply.github.com>
2023-11-30 23:24:26 -0800

    comments,window size

--- pu.py
+++ pu.py
@@ -6,6 +6,7 @@ from reportlab.pdfgen import canvas
 
 DATA_FILE = 'todo_lists.json'
 
+# Load lists from file
 def load_lists():
     try:
         with open(DATA_FILE, 'r') as file:
@@ -13,10 +14,12 @@ def load_lists():
     except FileNotFoundError:
         return {}
 
+# Save lists to file
 def save_lists(lists):
     with open(DATA_FILE, 'w') as file:
         json.dump(lists, file)
 
+# Add new list
 def add_list():
     list_name = simpledialog.askstring("Input", "Enter the new list name:", parent=window)
     if list_name:
@@ -24,10 +27,12 @@ def add_list():
         save_lists(lists)
         update_listbox()
 
+# Add item to list
 def add_item_to_list(list_name, item):
     lists[list_name].append(item)
     save_lists(lists)
 
+# Delete list
 def delete_list():
     list_name = listbox.get(tk.ACTIVE)
     if list_name and messagebox.askyesno("Confirm", f"Are you sure you want to delete '{list_name}'?"):
@@ -35,6 +40,7 @@ def delete_list():
         save_lists(lists)
         update_listbox()
 
+# Show items of selected list
 def show_items(event):
     list_name = listbox.get(listbox.curselection())
     if list_name:
@@ -47,12 +53,14 @@ def show_items(event):
         for item in lists[list_name]:
             items_listbox.insert(tk.END, item)
 
+        # Add item
         def add_item():
             item = simpledialog.askstring("Input", f"Enter a new item for list '{list_name}':", parent=items_window)
             if item:
                 add_item_to_list(list_name, item)
                 items_listbox.insert(tk.END, item)
 
+        # Delete item
         def delete_item():
             selected_index = items_listbox.curselection()
             if selected_index:
@@ -60,6 +68,7 @@ def show_items(event):
                 del lists[list_name][selected_index[0]]
                 save_lists(lists)
 
+        # Move item up
         def move_item_up():
             selected_index = items_listbox.curselection()
             if selected_index and selected_index[0] > 0:
@@ -68,6 +77,7 @@ def show_items(event):
                 save_lists(lists)
                 update_items_listbox()
 
+        # Move item down
         def move_item_down():
             selected_index = items_listbox.curselection()
             if selected_index and selected_index[0] < len(lists[list_name]) - 1:
@@ -76,11 +86,13 @@ def show_items(event):
                 save_lists(lists)
                 update_items_listbox()
 
+        # Update items listbox
         def update_items_listbox():
             items_listbox.delete(0, tk.END)
             for item in lists[list_name]:
                 items_listbox.insert(tk.END, item)
 
+        # Item management buttons
         btn_frame = tk.Frame(items_window)
         btn_frame.pack(side=tk.RIGHT, fill=tk.Y)
 
@@ -96,28 +108,37 @@ def show_items(event):
         move_down_button = tk.Button(btn_frame, text="Move Down", command=move_item_down)
         move_down_button.pack()
 
+# Update main listbox
 def update_listbox():
     listbox.delete(0, tk.END)
     for name in lists:
         listbox.insert(tk.END, name)
 
+# Placeholder for PDF saving
 def save_as_pdf(list_name, items):
     pass
 
+# Main window setup
 window = tk.Tk()
 window.title("To-Do List Application")
+window.geometry("500x500")
 
+# Main listbox
 listbox = tk.Listbox(window)
 listbox.pack()
 listbox.bind('<Double-Button-1>', show_items)
 
+# New list button
 add_list_button = tk.Button(window, text="Add New List", command=add_list)
 add_list_button.pack()
 
+# Delete list button
 delete_list_button = tk.Button(window, text="Delete List", command=delete_list)
 delete_list_button.pack()
 
+# Load and display lists
 lists = load_lists()
 update_listbox()
 
+# Start application
 window.mainloop()
